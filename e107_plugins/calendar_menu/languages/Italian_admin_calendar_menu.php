9ef9a69e48012dce97f0bd88de88dfbacea0dd53
root <root@fenice.serverkreos.eu>
2017-03-30 12:29:15 +0200

    +++ prime modifiche ed inserimento calendar_menu

---
+++ e107_plugins/calendar_menu/languages/Italian_admin_calendar_menu.php
@@ -0,0 +1,225 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Messages for admin pages of event calendar
+ *
+ * $Source: /cvs_backup/e107_0.8/e107_plugins/calendar_menu/languages/English_admin_calendar_menu.php,v $
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
+/**
+ *	e107 Event calendar plugin
+ *
+ *	Language file - 'admin' pages
+ *
+ *	@package	e107_plugins
+ *	@subpackage	event_calendar
+ *	@version 	$Id$;
+ */
+
+// TODO LAN REWRITE
+
+define('EC_ADLAN_1', "Calendario Eventi");
+
+define('EC_LAN_12', "Lunedì");
+define('EC_LAN_13', "Martedì");
+define('EC_LAN_14', "Mercoledì");
+define('EC_LAN_15', "Giovedì");
+define('EC_LAN_16', "Venerdì");
+define('EC_LAN_17', "Sabato");
+define('EC_LAN_18', "Domenica");
+define('EC_LAN_19', "Lun");
+define('EC_LAN_20', "Mar");
+define('EC_LAN_21', "Mer");
+define('EC_LAN_22', "Gio");
+define('EC_LAN_23', "Ven");
+define('EC_LAN_24', "Sab");
+define('EC_LAN_25', "Dom");
+define('EC_LAN_163', "Lista eventi");
+// If any 'EC_LAN_xxx' not found, look in language file for user pages.
+
+define("EC_ADLAN_A09", "Calendario Principale");
+define("EC_ADLAN_A10", "Configurazione");
+define("EC_ADLAN_A11", "Categorie");
+define("EC_ADLAN_A12", "Calendario");
+//define("EC_ADLAN_A13", "Edita");
+//define("EC_ADLAN_A14", "Nuovo");
+//define("EC_ADLAN_A15", "Elimina");
+//define("EC_ADLAN_A16", "Conferma");
+define("EC_ADLAN_A17", "Procedi");
+define("EC_ADLAN_A18", "Azione");
+define("EC_ADLAN_A19", "Amministra Categorie");
+define("EC_ADLAN_A20", "Categorie Calendario");
+define("EC_ADLAN_A21", "Nome Categoria");
+
+define("EC_ADLAN_A23", "Crea categoria");
+define("EC_ADLAN_A24", "Edita categoria");
+define("EC_ADLAN_A25", "Salva");
+define("EC_ADLAN_A26", "Categoria creata");
+define("EC_ADLAN_A27", "Impossibile creare categoria");
+define("EC_ADLAN_A28", "Modifiche salvate");
+define("EC_ADLAN_A29", "Impossibile salvare modifiche");
+define("EC_ADLAN_A30", "Categoria Eliminata");
+define("EC_ADLAN_A31", "Spunta il box per eliminare");
+define("EC_ADLAN_A32", "Impossibile eliminare questa categoria");
+define("EC_ADLAN_A33", "Nessuno definito");
+define("EC_ADLAN_A34", "Amministratore classe Calendario");
+define("EC_ADLAN_A35", "");
+
+define("EC_ADLAN_A59", "La Categoria è in uso. Non posso eliminarla.");
+
+define("EC_ADLAN_A80", "Visibile a");
+define("EC_ADLAN_A81", "Permetti sottoscrizioni");
+define("EC_ADLAN_A82", "Forza notifica a questa classe");
+define("EC_ADLAN_A83", "Anticipo giorni di notifica evento");
+define("EC_ADLAN_A84", "Messaggio Avanzato");
+define("EC_ADLAN_A85", "Messaggio del giorno");
+define("EC_ADLAN_A86", "Invia email");
+define("EC_ADLAN_A87", "Nessuno");
+define("EC_ADLAN_A88", "Solo avanzato");
+define("EC_ADLAN_A89", "Solo nel giorno");
+define("EC_ADLAN_A90", "Avanzato e nel giorno");
+define("EC_ADLAN_A91", "Soggetto Email");
+define("EC_ADLAN_A92", "Email da (nome)");
+define("EC_ADLAN_A93", "Email da indirizzo email");
+define("EC_ADLAN_A94", "Aggiungi una nuova classe di eventi");
+define("EC_ADLAN_A95", "Abilita sottoscrizioni");
+define("EC_ADLAN_A96", "Disabilitare per rimuovere il pulsante delle sottoscrizioni e non considerare i settaggi per le sottoscrizioni della categoria.");
+
+// Added from here 12.07.06 for forthcoming events and mailout upgrades
+
+define("EC_ADLAN_A100", "Eventi prossimi");
+define("EC_ADLAN_A101", "Giorni ricerca futura:");
+define("EC_ADLAN_A102", "Numero di eventi da visualizzare:");
+define("EC_ADLAN_A103", "Includi eventi ricorrenti:");
+define("EC_ADLAN_A104", "Titolo come link a lista eventi:");
+define("EC_ADLAN_A105", "Configura menu eventi prossimi");
+define("EC_ADLAN_A106", "Il Menu deve essere abilitato nella relativa area di amministrazione");
+define("EC_ADLAN_A107", "Non funzionerà oltre 59 giorni avanti");
+define("EC_ADLAN_A108", "Intestazione Menu");
+define("EC_ADLAN_A109", "Preferenze Eventi Prossimi aggiornate");
+define("EC_ADLAN_A110", "Solo giorni precedenti ");
+define("EC_ADLAN_A111", "Giorni precedenti e futuri");
+define("EC_ADLAN_A112", "Giorni precedenti e giorno corrente");
+define("EC_ADLAN_A113", "Giorni futuri, precedenti e giorno corrente");
+define("EC_ADLAN_A114", "Logging di Email");
+define("EC_ADLAN_A115", "Sommario");
+define("EC_ADLAN_A116", "Dettagli");
+define("EC_ADLAN_A117", "Messaggio del giorno o dei giorni precedenti");
+define("EC_ADLAN_A118", "Categorie da visualizzare");
+define("EC_ADLAN_A119", "Nessuna categoria definita o errore di lettura database");
+define("EC_ADLAN_A120", "Mostra le icone delle categorie nel menu");
+define("EC_ADLAN_A121", "Descrizione Categoria");
+define("EC_ADLAN_A122", "Riferimento orario Calendario");
+define("EC_ADLAN_A123", "Formato Ora Calendario");
+define("EC_ADLAN_A124", "Ora Corrente server: ");
+define("EC_ADLAN_A125", "Ora Corrente sito: ");
+define("EC_ADLAN_A126", "Ora Corrente user: ");
+define("EC_ADLAN_A127", "Determina il formato della visualizzazione ora nel calendario eventi.");
+define("EC_ADLAN_A128", "Il formato data è espresso nel box a destra");
+define("EC_ADLAN_A129", "Ora Sito");
+define("EC_ADLAN_A130", "Nome evento linkato a:");
+define("EC_ADLAN_A131", "Evento Calendario");
+define("EC_ADLAN_A132", "Sorgente Info URL");
+define("EC_ADLAN_A133", "Formato Data per eventi inseriti: ");
+define("EC_ADLAN_A134", "Level of logging to main admin log:");
+define("EC_ADLAN_A135", "Edita/elimina");
+define("EC_ADLAN_A136", "Tutte le modifiche");
+define("EC_ADLAN_A137", "Può coprire inserimenti, aggiornamenti e cancellazioni dall'elenco eventi");
+define("EC_ADLAN_A138", "Orari inizio/termine Evento con margine di 5-minuti");
+define("EC_ADLAN_A139", "(Riduce il n. di inserimenti in un menu a tendina)");
+define("EC_ADLAN_A140", "Mostra il n. di eventi per il mese corrente nel Menu Calendario");
+define("EC_ADLAN_A141", "Manutenzione");
+define("EC_ADLAN_A142", "Rimuovi eventi conclusi da più di x mesi");
+define("EC_ADLAN_A143", "dall'inizio del mese corrente.");
+define("EC_ADLAN_A144", "Manutenzione Calendario Eventi");
+define("EC_ADLAN_A145", "Cancella eventi conclusi");
+define("EC_ADLAN_A146", "Eventi conclusi oltre ");
+define("EC_ADLAN_A147", " cancellato");
+define("EC_ADLAN_A148", "Errore di parametro - nessuna cancellazione");
+define("EC_ADLAN_A149", "Nessun evento vecchio da cancellare oppure cancellazione fallita");
+define("EC_ADLAN_A150", "Conferma cancellazione eventi conclusi da più di  ");
+define("EC_ADLAN_A151", "e107 Web Site");
+define("EC_ADLAN_A152", "calendar@yoursite.com");
+define("EC_ADLAN_A153", "La Log directory deve essere creata manualmente - crea una sotto-directory 'log' fuori del tuo plugin Calendar_menu , con diritti di accesso '666'");
+define("EC_ADLAN_A154", "Non è possibile cambiare i permessi della directory log.");
+define("EC_ADLAN_A155", "I permessi della directory Log possono richiedere un aggiornamento manuale a 0666 o 0766, altrimenti il setup potrebbe non funzionare sul tuo server.");
+define("EC_ADLAN_A156", "Database aggiornato");
+define("EC_ADLAN_A157", "questa è una Rss feed del Calendario Eventi");
+define("EC_ADLAN_A158", "Impossibile creare log directory");
+define("EC_ADLAN_A159", "Cache Management");
+define("EC_ADLAN_A160", "(Funziona solo se la Cache è attivata)");
+define("EC_ADLAN_A161", "Svuota Cache del Calendario");
+define("EC_ADLAN_A162", "Conferma svuotanmento Cache");
+define("EC_ADLAN_A163", "Cache svuotata");
+define("EC_ADLAN_A164", "Aggiornamento completato");
+define("EC_ADLAN_A165", "Il link al Calendario Eventi è:");
+define("EC_ADLAN_A166", "Formato Data in Lista Eventi:");
+define("EC_ADLAN_A167", "Formato Data in Eventi prossimi:");
+define("EC_ADLAN_A168", "Il formato data è espresso nel box a destra");
+define("EC_ADLAN_A169", "Determina il formato Data nella Lista Eventi");
+define("EC_ADLAN_A170", "Determina il formato Data nel Menu Eventi prossimi");
+define("EC_ADLAN_A171", "Flag su eventi aggiunti/aggiornati");
+define("EC_ADLAN_A172", "Valore di partenza dall'aggiornamento in ore; zero per disabilitare");
+define('EC_ADLAN_A173', "Adesioni");
+define('EC_ADLAN_A174', "Non sono presenti adesioni");
+define('EC_ADLAN_A175', "UID");
+define('EC_ADLAN_A176', "Nome utente"); //FIXME Use Generic
+//define('EC_ADLAN_A177', "Category"); //FIXME Use Generic
+define('EC_ADLAN_A178', "Problemi");
+define('EC_ADLAN_A179', "Azioni");
+//define('EC_ADLAN_A180', "Deleted subscription record no "); //FIXME Use Generic
+//define('EC_ADLAN_A181', "Delete failed for record no "); //FIXME Use Generic
+define('EC_ADLAN_A182', "Total --NUM-- inserimenti nel database");
+define('EC_ADLAN_A183', "Visualizza il titolo evento al passaggio del mouse nel Menu Calendario");
+define('EC_ADLAN_A184', "potrebbe non funzionare con tutti i browsers");
+define('EC_ADLAN_A185', "Nessuno");
+define('EC_ADLAN_A186', "Aggiorna settaggi\ned inviati una test mail");
+define('EC_ADLAN_A187', "Test email inviata - ");
+define('EC_ADLAN_A188', "Errore durante l'invio della mail di test - ");
+define('EC_ADLAN_A189', "Se il messaggio viene lasciato vuoto, verrà utilizzato il messaggio di 'default' categoria");
+define('EC_ADLAN_A190', "Categoria di Default - i messaggi verranno inviati se non definiti in nessuna categoria");
+define('EC_ADLAN_A191', "Dettagli evento per la mail di test");
+define('EC_ADLAN_A192', "Test luogo evento");
+define('EC_ADLAN_A193', "Permetti agli utenti di visualizzare/stampare/PDF la lista");
+define('EC_ADLAN_A194', "Nessuno");
+define('EC_ADLAN_A195', "Visualizza/Stampa");
+define('EC_ADLAN_A196', "Visualizza/Stampa/PDF");
+define('EC_ADLAN_A197', "No class membership");
+define('EC_ADLAN_A198', "Utente Non Valido");
+define('EC_ADLAN_A199', "Vedi icone 'recenti'");
+define('EC_ADLAN_A200', "Editor degli eventi");
+define('EC_ADLAN_A201', 'BBCode (Standard)');
+define('EC_ADLAN_A202', 'BBCode con aiuto');
+define('EC_ADLAN_A203', 'WYSIWYG');
+define('EC_ADLAN_A204', 'Impostazioni Calendario aggiornate.');
+//define('EC_ADLAN_A205', 'Confirm Delete');
+define('EC_ADLAN_A206', 'This plugin is a fully featured event calendar with calendar menu.');
+define('EC_ADLAN_A207', 'Impostazioni Calendario');
+define('EC_ADLAN_A208', 'Gli Eventi possono essere aggiunti da:');
+define('EC_ADLAN_A209', 'Lista Eventi');
+define('EC_ADLAN_A210', 'Calendario');
+define('EC_ADLAN_A211', 'Classe Amministratori Calendario');
+define('EC_ADLAN_A212', 'La Settimana comincia con:');
+define('EC_ADLAN_A213', 'Show link to \'more information\' with events');
+define('EC_ADLAN_A214', 'Lunghezza Nome Giorno (caratteri)');
+define('EC_ADLAN_A215', 'Formato Data nell'header del calendario:');
+define('EC_ADLAN_A216', 'mese/anno');
+define('EC_ADLAN_A217', 'anno/mese');
+//define('EC_ADLAN_A218', 'Update Settings');
+define('EC_ADLAN_A219', 'Icona:');
+define('EC_ADLAN_A220', 'Scegli');
+define('EC_ADLAN_A221', 'Evento Calendario - Aggiungi evento ');
+define('EC_ADLAN_A222', 'Evento Calendario - Modifica evento ');
+define('EC_ADLAN_A223', 'Evento Calendario - Cancella evento ');
+define('EC_ADLAN_A224', 'Evento Calendario - Cancella molti');
+define('EC_ADLAN_A225', 'Evento Calendario - Aggiunta Multipla ');
+define('EC_ADLAN_A226', '');
+define('EC_ADLAN_A227', '');
